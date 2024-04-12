7b8edd5dcff03e8a1b2d4f283df453e126b31471
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-04-12 21:07:49 +0300

    Add BrandModel query dynamic search

---
+++ src/demoProjects/rentACar/Persistence/Repositories/ModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Persistence.Contexts;
+using Domain.Entities;
+
+namespace Persistence.Repositories
+{
+    public class ModelRepository : EfRepositoryBase<Model, BaseDbContext>, IModelRepository
+    {
+        public ModelRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
